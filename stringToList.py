147068886b85fcd30755a09cd8db8cd340b2b248
Niv Ben Abat <niv.benavat@gmail.com>
2019-04-01 09:54:20 +0300

    Add files via upload

---
+++ stringToList.py
@@ -0,0 +1,18 @@
+# Python program to convert string to list
+
+
+def convert(string):
+    return list(string.split(" "))
+
+str1 = "Pythin is love python is life"
+
+print(convert(str1))
+
+
+
+
+# ========== Output ==========
+# ['Pythin','is','love','python','is','life']
+
+  
+
