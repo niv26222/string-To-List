1162c770ca108edfee4a53e6e2c9f285c13aafe7
Niv Ben Abat <niv.benavat@gmail.com>
2019-04-01 09:54:45 +0300

    Update stringToList.py

--- stringToList.py
+++ stringToList.py
@@ -4,7 +4,7 @@
 def convert(string):
     return list(string.split(" "))
 
-str1 = "Pythin is love python is life"
+str1 = "Python is love python is life"
 
 print(convert(str1))
 
@@ -12,7 +12,7 @@ print(convert(str1))
 
 
 # ========== Output ==========
-# ['Pythin','is','love','python','is','life']
+# ['Python','is','love','python','is','life']
 
   
 
